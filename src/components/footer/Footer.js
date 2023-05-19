499763545e9e21d9260287d0f626415575b17ca7
ichiragtaluja <Ichiragtaluja@gmail.com>
2023-05-19 22:35:25 +0530

    style: add styles to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Footer.css";
+import { ImGithub } from "react-icons/im";
+import { SiLinkedin } from "react-icons/si";
+import { BsTwitter } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   const copyrightYear = new Date().getFullYear();
@@ -7,6 +11,17 @@ export const Footer = () => {
   return (
     <div className="footer">
       <small> &copy; {copyrightYear} Chirag Taluja</small>
+      <div className="social-links">
+        <Link to="https://twitter.com/ichiragtaluja" target="_blank">
+          <BsTwitter color={"white"}/>
+        </Link>
+        <Link to="https://github.com/ichiragtaluja" target="_blank">
+          <ImGithub color={"white"}/>
+        </Link>
+        <Link to="https://www.linkedin.com/in/ichiragtaluja" target="_blank">
+          <SiLinkedin color={"white"}/>
+        </Link>
+      </div>
     </div>
   );
 };
