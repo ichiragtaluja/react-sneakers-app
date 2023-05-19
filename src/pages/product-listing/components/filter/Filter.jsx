ffc3cbce1895312aa953366f86e8d64ecb0d719e
ichiragtaluja <Ichiragtaluja@gmail.com>
2023-05-20 04:15:55 +0530

    style: made navbar mobile responsive

--- src/pages/product-listing/components/filter/Filter.jsx
+++ src/pages/product-listing/components/filter/Filter.jsx
@@ -25,13 +25,17 @@ export const Filter = () => {
               : "filter-header filter-header-mobile-open"
           }
         >
-          <span onClick={() => setIsFilterMenuOn(!isFilterMenuOn)}>
+          <span
+            className="close-tab"
+            onClick={() => setIsFilterMenuOn(!isFilterMenuOn)}
+          >
             {!isFilterMenuOn ? <TbAdjustmentsHorizontal /> : <RxCross2 />}
           </span>
           <h2>Filters</h2>
 
           {isFilterMenuOn && (
             <button
+              className="reset-btn"
               onClick={() =>
                 dispatch({
                   type: "RESET",
@@ -56,9 +60,82 @@ export const Filter = () => {
               : "filter-types-container"
           }
         >
+          <div className="price-container">
+            <h3>Price</h3>
+            <div className="price-input-container">
+              <label htmlFor="below-200">
+                Below $200
+                <input
+                  checked={state.filters.price.find((price) =>
+                    price.min === 0 ? true : false
+                  )}
+                  onChange={() =>
+                    dispatch({
+                      type: "ADD_PRICE",
+                      payload: { min: 0, max: 200 },
+                    })
+                  }
+                  id="below-200"
+                  type="checkbox"
+                />
+              </label>
+
+              <label htmlFor="201-999">
+                $201 - $999
+                <input
+                  checked={state.filters.price.find((price) =>
+                    price.min === 201 ? true : false
+                  )}
+                  onChange={() =>
+                    dispatch({
+                      type: "ADD_PRICE",
+                      payload: { min: 201, max: 999 },
+                    })
+                  }
+                  id="201-999"
+                  type="checkbox"
+                />
+              </label>
+
+              <label htmlFor="1000-1999">
+                $1000 - $1999
+                <input
+                  checked={state.filters.price.find((price) =>
+                    price.min === 1000 ? true : false
+                  )}
+                  onChange={() =>
+                    dispatch({
+                      type: "ADD_PRICE",
+                      payload: { min: 1000, max: 1999 },
+                    })
+                  }
+                  id="1000-1999"
+                  type="checkbox"
+                />
+              </label>
+
+              <label htmlFor="above 2000">
+                Over $2000
+                <input
+                  checked={state.filters.price.find((price) =>
+                    price.min === 2000 ? true : false
+                  )}
+                  onChange={() =>
+                    dispatch({
+                      type: "ADD_PRICE",
+                      payload: { min: 2000, max: 5000 },
+                    })
+                  }
+                  id="above 2000"
+                  type="checkbox"
+                />
+              </label>
+            </div>
+          </div>
+
           <div className="ratings-container ratings-container-mobile">
             <h3>Ratings</h3>
-            <div>
+            <div className="input-range">
               <datalist id="markers">
                 <option label="0" value="0">
                   0
@@ -90,110 +167,55 @@ export const Filter = () => {
 
           <div className="category-container">
             <h3>Categories</h3>
-            <div>
+            <div className="category-input-container">
               {state.allCategories?.map(({ categoryName }) => (
-                <div key={categoryName}>
-                  <label
-                    htmlFor={`category-${categoryName}`}
-                  >{`${categoryName}'s wear`}</label>
-                  <input
-                    checked={state.filters.categories.includes(categoryName)}
-                    onChange={() =>
-                      dispatch({
-                        type: "ADD_CATEGORIES",
-                        payload: categoryName,
-                      })
-                    }
-                    id={`category-${categoryName}`}
-                    type="checkbox"
-                  />
+                <div className="category-input-container" key={categoryName}>
+                  <label htmlFor={`category-${categoryName}`}>
+                    {`${categoryName}'s wear`}
+                    <input
+                      checked={state.filters.categories.includes(categoryName)}
+                      onChange={() =>
+                        dispatch({
+                          type: "ADD_CATEGORIES",
+                          payload: categoryName,
+                        })
+                      }
+                      id={`category-${categoryName}`}
+                      type="checkbox"
+                    />
+                  </label>
                 </div>
               ))}
-            </div>
-          </div>
-
-          <div className="price-container">
-            <h3>Price</h3>
-            <div>
-              <label htmlFor="below-200">Below $200</label>
-              <input
-                checked={state.filters.price.find((price) =>
-                  price.min === 0 ? true : false
-                )}
-                onChange={() =>
-                  dispatch({ type: "ADD_PRICE", payload: { min: 0, max: 200 } })
-                }
-                id="below-200"
-                type="checkbox"
-              />
-              <label htmlFor="201-999">$201 - $999</label>
-              <input
-                checked={state.filters.price.find((price) =>
-                  price.min === 201 ? true : false
-                )}
-                onChange={() =>
-                  dispatch({
-                    type: "ADD_PRICE",
-                    payload: { min: 201, max: 999 },
-                  })
-                }
-                id="201-999"
-                type="checkbox"
-              />
-              <label htmlFor="1000-1999">$1000 - $1999</label>
-              <input
-                checked={state.filters.price.find((price) =>
-                  price.min === 1000 ? true : false
-                )}
-                onChange={() =>
-                  dispatch({
-                    type: "ADD_PRICE",
-                    payload: { min: 1000, max: 1999 },
-                  })
-                }
-                id="1000-1999"
-                type="checkbox"
-              />
-              <label htmlFor="above 2000">Over $2000</label>
-              <input
-                checked={state.filters.price.find((price) =>
-                  price.min === 2000 ? true : false
-                )}
-                onChange={() =>
-                  dispatch({
-                    type: "ADD_PRICE",
-                    payload: { min: 2000, max: 5000 },
-                  })
-                }
-                id="above 2000"
-                type="checkbox"
-              />
             </div>
           </div>
 
           <div className="sorting-container">
             <h3>Sort by price</h3>
 
-            <div>
-              <label htmlFor="high-to-low">Price-high to low</label>
-              <input
-                onChange={() =>
-                  dispatch({ type: "ADD_SORT", payload: "highToLow" })
-                }
-                name="sort"
-                id="high-to-low"
-                type="radio"
-              />
+            <div className="sorting-input-container">
+              <label htmlFor="high-to-low">
+                Price-high to low
+                <input
+                  onChange={() =>
+                    dispatch({ type: "ADD_SORT", payload: "highToLow" })
+                  }
+                  name="sort"
+                  id="high-to-low"
+                  type="radio"
+                />
+              </label>
 
-              <label htmlFor="low-to-high">Price-low to high</label>
-              <input
-                onChange={() =>
-                  dispatch({ type: "ADD_SORT", payload: "lowToHigh" })
-                }
-                name="sort"
-                id="low-to-high"
-                type="radio"
-              />
+              <label htmlFor="low-to-high">
+                Price-low to high
+                <input
+                  onChange={() =>
+                    dispatch({ type: "ADD_SORT", payload: "lowToHigh" })
+                  }
+                  name="sort"
+                  id="low-to-high"
+                  type="radio"
+                />
+              </label>
             </div>
           </div>
         </div>
